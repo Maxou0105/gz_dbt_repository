ab6de8540db23cb0595c3b299e43e71e88d5a1cb
HAMON Maximilien <hamon.maximilien@gmail.com>
2024-06-19 08:36:15 +0000

    marge modif

--- models/intermediaire/int_orders_margin.sql
+++ models/intermediaire/int_orders_margin.sql
@@ -1,1 +1,14 @@
-select * from {{ref('int_sales_margin')}}
+SELECT
+date_date, 
+orders_id,
+round(sum(revenue),2) as revenue,
+round(sum(quantity),2) as quantity,
+round(sum(purchase_cost),2) as purchase_cost,
+round(sum(margin),2) as margin
+
+FROM {{ref('int_sales_margin')}}
+GROUP BY
+date_date,
+orders_id
+HAVING orders_id = 1002560
+ORDER BY date_date
--- models/intermediaire/int_sales_margin.sql
+++ models/intermediaire/int_sales_margin.sql
@@ -1,6 +1,6 @@
 select 
 sales.*,
-round(revenue - cast(purchase_price as FLOAT64)*quantity,2) as margin,
+round(revenue - (quantity*cast(purchase_price as FLOAT64)),2) as margin,
 round((quantity*cast(purchase_price as FLOAT64)),2) as purchase_cost,
 CAST(prod.purchase_price as FLOAT64) as purchase_price
 from {{ref('stg_raw__sales')}}as sales 
