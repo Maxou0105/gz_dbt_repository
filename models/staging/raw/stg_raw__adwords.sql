063aca5d1b2c6ee8e1d740698bb42b34692d6ce0
HAMON Maximilien <hamon.maximilien@gmail.com>
2024-06-19 13:09:47 +0000

    STAGE1.2

--- models/staging/raw/stg_raw__adwords.sql
+++ models/staging/raw/stg_raw__adwords.sql
@@ -12,7 +12,7 @@ renamed as (
         date_date,
         paid_source,
         campaign_key,
-        campgn_name as campaign_name
+        campgn_name as campaign_name,
         cast(ads_cost as FLOAT64) as ads_cost,
         impression,
         click
